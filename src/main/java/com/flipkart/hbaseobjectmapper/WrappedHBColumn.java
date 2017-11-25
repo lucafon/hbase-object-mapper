29eb82f2c4d013558e4b5c3f45f767f6f6526ee1
ConsFontaniliLuca <cons.fontanililuca@sia.eu>
2017-11-25 12:26:59 +0100

    multiId: added HBColumnMultiId in order to map a List of object to different cells having the family and qualifier set to the values of the annotations attribute. The 'method' attribute defines the method name to invoke via Reflection in order to retrieve the value of the attrbute of the class in the list to map.

--- src/main/java/com/flipkart/hbaseobjectmapper/WrappedHBColumn.java
+++ src/main/java/com/flipkart/hbaseobjectmapper/WrappedHBColumn.java
@@ -1,7 +1,7 @@
 package com.flipkart.hbaseobjectmapper;
 
 
-import com.flipkart.hbaseobjectmapper.exceptions.BothHBColumnAnnotationsPresentException;
+import com.flipkart.hbaseobjectmapper.exceptions.TooManyHBColumnAnnotationsPresentException;
 import com.flipkart.hbaseobjectmapper.exceptions.DuplicateCodecFlagForColumnException;
 
 import java.lang.reflect.Field;
@@ -13,8 +13,8 @@ import java.util.Map;
  * A wrapper class for {@link HBColumn} and {@link HBColumnMultiVersion} annotations (for internal use only)
  */
 class WrappedHBColumn {
-    private final String family, column;
-    private final boolean multiVersioned, singleVersioned;
+    private final String family, column, method;
+    private final boolean multiVersioned, singleVersioned, multiId;
     private final Class annotationClass;
     private final Map<String, String> codecFlags;
     private final Field field;
@@ -23,34 +23,61 @@ class WrappedHBColumn {
         this.field = field;
         HBColumn hbColumn = field.getAnnotation(HBColumn.class);
         HBColumnMultiVersion hbColumnMultiVersion = field.getAnnotation(HBColumnMultiVersion.class);
-        if (hbColumn != null && hbColumnMultiVersion != null) {
-            throw new BothHBColumnAnnotationsPresentException(field);
-        }
+        HBColumnMultiId hbColumnMultiId = field.getAnnotation(HBColumnMultiId.class);
+        checkSingleAnnotationPResent(hbColumn, hbColumnMultiVersion, hbColumnMultiId);
+        
         if (hbColumn != null) {
             family = hbColumn.family();
             column = hbColumn.column();
+            method = null;
             singleVersioned = true;
             multiVersioned = false;
+            multiId = false;
             annotationClass = HBColumn.class;
             codecFlags = toMap(hbColumn.codecFlags());
         } else if (hbColumnMultiVersion != null) {
             family = hbColumnMultiVersion.family();
             column = hbColumnMultiVersion.column();
+            method = null;
             singleVersioned = false;
             multiVersioned = true;
+            multiId = false;
             annotationClass = HBColumnMultiVersion.class;
             codecFlags = toMap(hbColumnMultiVersion.codecFlags());
-        } else {
+        } else if (hbColumnMultiId != null){
+        		family = hbColumnMultiId.family();
+        		column = null;
+        		method = hbColumnMultiId.method();
+        		singleVersioned = false;
+        		multiVersioned = false;
+        		multiId = true;
+        		annotationClass = HBColumnMultiId.class;
+        		codecFlags = toMap(hbColumnMultiId.codecFlags());
+        }
+        else {
             family = null;
             column = null;
+            method = null;
             singleVersioned = false;
             multiVersioned = false;
+            multiId = false;
             annotationClass = null;
             codecFlags = null;
         }
     }
 
-    private Map<String, String> toMap(Flag[] codecFlags) {
+    private void checkSingleAnnotationPResent(HBColumn hbColumn, HBColumnMultiVersion hbColumnMultiVersion,
+			HBColumnMultiId hbColumnMultiId) {
+		if(hbColumn != null && (hbColumnMultiVersion != null || hbColumnMultiId!= null)) {
+			throw new TooManyHBColumnAnnotationsPresentException(field);
+		} else if(hbColumnMultiVersion != null && hbColumnMultiId != null) {
+			throw new TooManyHBColumnAnnotationsPresentException(field);
+		}
+	}
+
+	
+
+	private Map<String, String> toMap(Flag[] codecFlags) {
         Map<String, String> flagsMap = new HashMap<>(codecFlags.length, 1.0f);
         for (Flag flag : codecFlags) {
             String previousValue = flagsMap.put(flag.name(), flag.value());
@@ -68,13 +95,17 @@ class WrappedHBColumn {
     public String column() {
         return column;
     }
+    
+    public String method() {
+    		return method;
+    }
 
     public Map<String, String> codecFlags() {
         return codecFlags;
     }
 
     public boolean isPresent() {
-        return singleVersioned || multiVersioned;
+        return singleVersioned || multiVersioned || multiId;
     }
 
     public boolean isMultiVersioned() {
@@ -83,6 +114,10 @@ class WrappedHBColumn {
 
     public boolean isSingleVersioned() {
         return singleVersioned;
+    }
+    
+    public boolean isMultiId() {
+    		return multiId;
     }
 
     public String getName() {
